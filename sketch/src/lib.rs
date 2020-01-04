e4e5bca4c21cca776b8d71d303879b2cc4229284
Caleb Daniels <CJordanDaniels@gmail.com>
2020-01-04 15:42:58 -0600

    Refactor synchronization objects

--- sketch/src/lib.rs
+++ sketch/src/lib.rs
@@ -49,10 +49,7 @@ pub struct Renderer {
     frame_buffers: FrameBuffer,
     command_pool: CommandPool,
     command_buffers: CommandBuffer,
-    image_available_semaphores: Vec<vk::Semaphore>,
-    render_finished_semaphores: Vec<vk::Semaphore>,
-    in_flight_fences: Vec<vk::Fence>,
-    images_in_flight: RefCell<Vec<vk::Fence>>,
+    sync_objects: SyncObjects,
     current_frame: Cell<usize>,
 }
 
@@ -69,24 +66,7 @@ impl Renderer {
         let framebuffer = FrameBuffer::new(logical_device.vulkan_object(), &swapchain, &render_pass);
         let command_pool = CommandPool::new(logical_device.vulkan_object(), &physical_device);
         let command_buffers = CommandBuffer::new(logical_device.vulkan_object(), &command_pool, framebuffer.vulkan_object().len() as u32);
-
-        let mut image_available_semaphores: Vec<vk::Semaphore> = Vec::new();
-        let mut render_finished_semaphores: Vec<vk::Semaphore> = Vec::new();
-        let mut in_flight_fences: Vec<vk::Fence> = Vec::new();
-        let images_in_flight: RefCell<Vec<vk::Fence>> = RefCell::new(Vec::new());
-
-        let semaphore_info = vk::SemaphoreCreateInfo::default();
-        let fence_info = vk::FenceCreateInfo::builder().flags(vk::FenceCreateFlags::SIGNALED).build();
-
-        for _ in 0..MAX_FRAMES_IN_FLIGHT {
-            unsafe {
-                in_flight_fences.push(logical_device.vulkan_object().create_fence(&fence_info, None).unwrap());
-                image_available_semaphores.push(logical_device.vulkan_object().create_semaphore(&semaphore_info, None).unwrap());
-                render_finished_semaphores.push(logical_device.vulkan_object().create_semaphore(&semaphore_info, None).unwrap());
-            }
-        }
-
-        images_in_flight.borrow_mut().resize(swapchain.images().len(), vk::Fence::null());
+        let sync_objects = SyncObjects::new(logical_device.vulkan_object(), MAX_FRAMES_IN_FLIGHT, swapchain.images().len());
 
         Renderer {
             _entry: entry,
@@ -100,10 +80,7 @@ impl Renderer {
             frame_buffers: framebuffer,
             command_pool: command_pool,
             command_buffers: command_buffers,
-            image_available_semaphores: image_available_semaphores,
-            render_finished_semaphores: render_finished_semaphores,
-            in_flight_fences: in_flight_fences,
-            images_in_flight: images_in_flight,
+            sync_objects: sync_objects,
             current_frame: Cell::new(0),
         }
     }
@@ -135,37 +112,37 @@ impl Renderer {
 
     pub fn draw_frame(&self) {
         unsafe {
-            self.get_device().wait_for_fences(&[self.in_flight_fences[self.current_frame.get()]], true, std::u64::MAX).unwrap();
+            self.get_device().wait_for_fences(&[self.sync_objects.in_flight_fences[self.current_frame.get()]], true, std::u64::MAX).unwrap();
         }
 
         let (image_index, _) = unsafe {
             self.swapchain
                 .get_loader()
-                .acquire_next_image(*self.swapchain.vulkan_object(), std::u64::MAX, self.image_available_semaphores[self.current_frame.get()], vk::Fence::null())
+                .acquire_next_image(*self.swapchain.vulkan_object(), std::u64::MAX, self.sync_objects.image_available_semaphores[self.current_frame.get()], vk::Fence::null())
                 .unwrap()
         };
 
-        if self.images_in_flight.borrow()[image_index as usize] != vk::Fence::null() {
+        if self.sync_objects.images_in_flight.borrow()[image_index as usize] != vk::Fence::null() {
             unsafe {
-                self.get_device().wait_for_fences(&[self.images_in_flight.borrow()[image_index as usize]], true, std::u64::MAX).unwrap();
+                self.get_device().wait_for_fences(&[self.sync_objects.images_in_flight.borrow()[image_index as usize]], true, std::u64::MAX).unwrap();
             }
         }
-        self.images_in_flight.borrow_mut()[image_index as usize] = self.in_flight_fences[self.current_frame.get()];
+        self.sync_objects.images_in_flight.borrow_mut()[image_index as usize] = self.sync_objects.in_flight_fences[self.current_frame.get()];
 
         let submit_info = vk::SubmitInfo::builder()
-            .wait_semaphores(&[self.image_available_semaphores[self.current_frame.get()]])
+            .wait_semaphores(&[self.sync_objects.image_available_semaphores[self.current_frame.get()]])
             .wait_dst_stage_mask(&[vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT])
             .command_buffers(&[self.command_buffers.vulkan_object()[image_index as usize]])
-            .signal_semaphores(&[self.render_finished_semaphores[self.current_frame.get()]])
+            .signal_semaphores(&[self.sync_objects.render_finished_semaphores[self.current_frame.get()]])
             .build();
 
         unsafe {
-            self.get_device().reset_fences(&[self.in_flight_fences[self.current_frame.get()]]).unwrap();
-            self.get_device().queue_submit(*self.logical_device.graphics_queue(), &[submit_info], self.in_flight_fences[self.current_frame.get()]).unwrap();
+            self.get_device().reset_fences(&[self.sync_objects.in_flight_fences[self.current_frame.get()]]).unwrap();
+            self.get_device().queue_submit(*self.logical_device.graphics_queue(), &[submit_info], self.sync_objects.in_flight_fences[self.current_frame.get()]).unwrap();
         };
 
         let present_info = vk::PresentInfoKHR::builder()
-            .wait_semaphores(&[self.render_finished_semaphores[self.current_frame.get()]])
+            .wait_semaphores(&[self.sync_objects.render_finished_semaphores[self.current_frame.get()]])
             .swapchains(&[*self.swapchain.vulkan_object()])
             .image_indices(&[image_index])
             .build();
@@ -192,9 +169,9 @@ impl Drop for Renderer {
 
         for i in 0..MAX_FRAMES_IN_FLIGHT {
             unsafe {
-                self.get_device().destroy_fence(self.in_flight_fences[i], None);
-                self.get_device().destroy_semaphore(self.image_available_semaphores[i], None);
-                self.get_device().destroy_semaphore(self.render_finished_semaphores[i], None);
+                self.get_device().destroy_fence(self.sync_objects.in_flight_fences[i], None);
+                self.get_device().destroy_semaphore(self.sync_objects.image_available_semaphores[i], None);
+                self.get_device().destroy_semaphore(self.sync_objects.render_finished_semaphores[i], None);
             }
         }
 
@@ -213,4 +190,40 @@ trait VulkanObject {
     type Object;
     fn vulkan_object(&self) -> &Self::Object;
     fn cleanup(&self, _renderer: &Renderer);
+}
+
+struct SyncObjects {
+    image_available_semaphores: Vec<vk::Semaphore>,
+    render_finished_semaphores: Vec<vk::Semaphore>,
+    in_flight_fences: Vec<vk::Fence>,
+    images_in_flight: RefCell<Vec<vk::Fence>>,
+}
+
+impl SyncObjects {
+    fn new(device: &Device, max_frames: usize, num_images: usize) -> Self {
+        let mut image_available_semaphores: Vec<vk::Semaphore> = Vec::new();
+        let mut render_finished_semaphores: Vec<vk::Semaphore> = Vec::new();
+        let mut in_flight_fences: Vec<vk::Fence> = Vec::new();
+        let images_in_flight: RefCell<Vec<vk::Fence>> = RefCell::new(Vec::new());
+
+        let semaphore_info = vk::SemaphoreCreateInfo::default();
+        let fence_info = vk::FenceCreateInfo::builder().flags(vk::FenceCreateFlags::SIGNALED).build();
+
+        for _ in 0..max_frames {
+            unsafe {
+                in_flight_fences.push(device.create_fence(&fence_info, None).unwrap());
+                image_available_semaphores.push(device.create_semaphore(&semaphore_info, None).unwrap());
+                render_finished_semaphores.push(device.create_semaphore(&semaphore_info, None).unwrap());
+            }
+        }
+
+        images_in_flight.borrow_mut().resize(num_images, vk::Fence::null());
+
+        SyncObjects {
+            image_available_semaphores,
+            render_finished_semaphores,
+            in_flight_fences,
+            images_in_flight,
+        }
+    }
 }
